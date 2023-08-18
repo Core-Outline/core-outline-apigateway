feec3379c0fa0939b6ffbdac59321bd20f983285
Ubuntu <ubuntu@ip-172-31-25-213.ec2.internal>
2023-08-18 10:33:28 +0000

    latest

--- app_container/scripts/start_servers.sh
+++ app_container/scripts/start_servers.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
+cd /home/ubuntu/api
+gunicorn --bind 0.0.0.0:5000 app:app &
 
-cd D://Arithmetica/core-outline-apigateway
-python -m flask run -p 5000 &
-
-cd D://Arithmetica/core-outline-mongodb-atlas
+cd /home/ubuntu/mongodb
 npm run dev &
 
-cd D://Arithmetica/core-outline-mysql-server
-python -m flask run -p 6000 &
+cd /home/ubuntu/mysql
+python3 -m flask run -p 6000 &
 
-cd D://Arithmetica/core-outline-ml-server
-python -m flask run -p 3000 &
+cd /home/ubuntu/ml
+pyt -m flask run -p 3000 &
 
-cd D://Arithmetica/core-outline-social-media
-python -m flask run -p 7000 &
+# cd D://Arithmetica/core-outline-social-media
+# python -m flask run -p 7000 &
 
 wait
--- config/sever_config.py
+++ config/sever_config.py
@@ -10,12 +10,12 @@ load_dotenv(dotenv_path=env_path)
 env_var = os.environ
 
 servers = {
-    "mongodb": 'mongodb',
-    "mysql": 'MYSQL_SERVER',
-    "csv": 'CSV_SERVER',
-    "ml": 'ML_SERVER',
-    "postgress": 'POSTGRESS_SERVER',
-    "social_media": 'SOCIAL_MEDIA_SERVER'
+    "mongodb": 'http://127.0.0.1:4000',
+    "mysql": 'http://127.0.0.1:6000',
+    # "csv": 'CSV_SERVER',
+    "ml": 'http://127.0.0.1:3000',
+    # "postgress": 'POSTGRESS_SERVER',
+    # "social_media": 'SOCIAL_MEDIA_SERVER'
 }
 
 
